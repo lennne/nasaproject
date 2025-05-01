9dd61ea87443d5f13eb23cc468e856487a25059f
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2025-05-01 13:01:34 -0230

    minor update

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets.router');
 
@@ -8,13 +9,22 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+
+//enable cors(cross origin) policy 
 app.use(cors(
     {   
     origin: 'http://localhost:3000',  
 }
 ));
+app.use(morgan('combined'));
+
+//process json from requests
 app.use(express.json());
+
+//api requests
 app.use(planetsRouter); 
+
+//serve files and folders in the public folder
 app.use(express.static(path.join(__dirname,'..', 'public')));
 
 module.exports = app;
