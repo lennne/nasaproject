b71cee00402c7cb4aa84389e07fb5b7c0a21096a
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2025-05-03 23:35:33 -0230

    minor update: testing post request

--- server/src/routes/launches.test.js
+++ server/src/routes/launches.test.js
@@ -3,21 +3,46 @@ const app = require('../app');
 
 
 describe('GET /launch', () => {
-    const response = request(app)
+    test('It should respond with 200 ', async () => {
+    const response = await request(app)
                         .get('/launch') 
                         .expect('Content-Type', /json/)
                         .expect(200);
+                    
+                    })
 
 });
 
 describe('POST /launch', () => {
-    test('It should respond with 200 success', () => {
-        const response = 200;
-        expect(response).toBe(200);
+    const launchData = {
+        mission: "ZTM15S",
+        rocket: "ZTM EXPERIMENTAL IS1",
+        target: "Kepler-186 f",
+        launchDate: "January 17, 2030"     
+    }
+
+    const launchDataWithoutDate = {
+        mission: "ZTM15S",
+        rocket: "ZTM EXPERIMENTAL IS1",
+        target: "Kepler-186 f",
+    }
+
+
+    test('It should responde with 201 created', async () => {
+
+    //testing a post request
+    const response = await request(app)
+                            .post('/launch')
+                            .send(launchData)
+                            .expect('Content-Type', /json/)
+                            .expect(201);
+
+    const requestDate = new Date(launchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(requestDate).toBe(responseDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
     })
-
-    
-
     test('It should catch missing requirements', () => {})
     test('It should catch invalid dates', () => {});
 })
